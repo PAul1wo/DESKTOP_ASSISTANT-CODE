7d419a0e50a353780831484dde72f05f38a84923
PAul1wo <67853781+PAul1wo@users.noreply.github.com>
2020-10-05 19:34:11 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -4,6 +4,13 @@ import datetime
 import os
 import cv2
 import random
+from requests import  get
+import webbrowser
+import pywhatkit
+import smtplib
+import sys
+
+
 
 
 engine = pyttsx3.init('sapi5') #text to voice
@@ -30,9 +37,20 @@ def take_commnad(): #Voice - > text
         print(f"User Said!:{query}")
 
     except Exception as e:
-        speak("Sorry I did Not get it Please Say that again...")
+        speak("Sorry I did Not get it , Please Say that again...")
         return "none"
     return query
+
+def SendEmail(to,content):  # to send Email
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.login('','')
+    server.sendmail('your email id',to,content)
+    server.close()
+
+
+
 
 def wishing():
     hour = int(datetime.datetime.now().hour)
@@ -72,11 +90,54 @@ if __name__ == "__main__":
             cap.release()
             cv2.destroyAllWindows()
 
-        elif "play music " in query: # Play Music
+        elif "play music" in query: # Play Music
             music_dir = "D:\\Musics"
             songs = os.listdir(music_dir)  # music converted into a list
             rd = random.choice(songs)
             for song in songs:
                 if song.endswith('.mp3'):
                     os.startfile(os.path.join(music_dir, song))
+
+
+        elif "ip address" in query: #Ip address of the particular network!
+            ip = get('https://api.ipify.org').text
+            speak(f'your ip address is {ip}')
+
+
+
+        elif "open Youtube" in query:
+            webbrowser.open("www.youtube.com")
+
+        elif "open facebook" in query:
+            webbrowser.open("www.facebook.com")
+
+        elif "Search google" in query:
+            speak('What should i search in google')
+            cm = take_commnad().lower()
+            webbrowser.open(f"{cm}")
+
+        elif "Play song on youtube" in query:
+            pywhatkit.playonyt("little girl")
+
+        elif "send email" in query:
+            try:
+                speak('what should i write in the email')
+                content = take_commnad().lower()
+                to = ""
+                SendEmail(to,content)
+                speak('Email has been send')
+
+            except Exception as e:
+                print(e)
+                speak('Sorry , I was not able to send the Email')
+
+        elif "close" in query:
+            speak('thanks for using me')
+            sys.exit()
+
+        speak('Can I help you ,  with Something else')
+
+
+
+
 
