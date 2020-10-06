cb2320235243d8b4543bc5175a09cc3f1660b9ef
PAul1wo <67853781+PAul1wo@users.noreply.github.com>
2020-10-06 18:19:21 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -4,140 +4,197 @@ import datetime
 import os
 import cv2
 import random
-from requests import  get
+from requests import get
+import wikipedia
 import webbrowser
-import pywhatkit
+import pywhatkit as kit
 import smtplib
 import sys
+import time
+import pyjokes
+
+engine = pyttsx3.init('sapi5')          #text to voice
+voices = engine.getProperty('voices');
+# print(voices[0].id)
+engine.setProperty('voices', voices[len(voices) - 1].id)
 
 
-
-
-engine = pyttsx3.init('sapi5') #text to voice
-voices = engine.getProperty('voices')
-engine.setProperty('voices' , voices[0].id)
-
-
-def speak(audio):   #text - > speech
+# text to speech
+def speak(audio):
     engine.say(audio)
     print(audio)
     engine.runAndWait()
 
-def take_commnad(): #Voice - > text
 
+# To convert voice into text
+def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening....")
-        r.pause_threshold  = 1  # Program won't stop Until 1 second
-        audio = r.listen(source,timeout=8,phrase_time_limit=12)
+        print("listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source, timeout=8, phrase_time_limit=12)
 
     try:
-        print("Recognizing............")
-        query = r.recognize_google(audio,language = 'en-in')
-        print(f"User Said!:{query}")
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
 
     except Exception as e:
-        speak("Sorry I did Not get it , Please Say that again...")
+        speak("Say that again please...")
         return "none"
     return query
 
-def SendEmail(to,content):  # to send Email
-    server = smtplib.SMTP('smtp.gmail.com',587)
+
+# to wish
+def wish():
+    hour = int(datetime.datetime.now().hour)  #Current time
+    tt = time.strftime("%I:%M %p")
+
+    if hour >= 0 and hour <= 12:
+        speak(f"good morning, its {tt}")
+    elif hour >= 12 and hour <= 18:
+        speak(f"good afternoon, its {tt}")
+    else:
+        speak(f"good evening, its {tt}")
+    speak("i am jarvis sir. please tell me how may i help you")
+
+
+# to send email
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('','')
-    server.sendmail('your email id',to,content)
+    server.login('YOUR EMAIL ADDRESS', 'YOUR PASSWORD')
+    server.sendmail('YOUR EMAIL ADDRESS', to, content)
     server.close()
 
 
-
-
-def wishing():
-    hour = int(datetime.datetime.now().hour)
-
-    if hour >=0 and hour <=12:
-        speak("good morning")
-    elif hour >12 and hour <=18:
-        speak("good afternoon")
-    else:
-        speak("good evening")
-    speak('Hope Your Safe and Sound . How Can i help you')
-
-if __name__ == "__main__":
-    wishing()
+if __name__ == "__main__":  # main program
+    wish()
     while True:
+        # if 1:
 
-        query = take_commnad().lower() # user input storing in Query
+        query = takecommand().lower()
 
-        #logic building for Tasks
+        # logic building for tasks
 
         if "open notepad" in query:
-            npath = "C:\\WINDOWS\\system32\\notepad.exe"
+            npath = "C:\\Windows\\system32\\notepad.exe"
             os.startfile(npath)
 
-        elif "open command prompt" in query:
-            cpath = "C:\\WINDOWS\\system32\\cmd.exe"
-            os.startfile(cpath)
+        elif 'hi' in query or 'hello' in query:
+            speak('Hello sir, how may I help you?')
 
-        elif "open camera" in query: #Open Camera
-            cap = cv2.VideoCapture(0) # 0 -> for internal Camera
+
+
+        elif "open command prompt" in query:
+            os.system("start cmd")
+
+        elif "open camera" in query:
+            cap = cv2.VideoCapture(0)  # 0 for internal Camera
             while True:
                 ret, img = cap.read()
                 cv2.imshow('webcam', img)
-                k = cv2.waitKey(30)
-                if k==20:
+                k = cv2.waitKey(50)
+                if k == 27:
                     break;
             cap.release()
             cv2.destroyAllWindows()
 
-        elif "play music" in query: # Play Music
-            music_dir = "D:\\Musics"
-            songs = os.listdir(music_dir)  # music converted into a list
+
+
+        elif "play music" in query:
+            speak('Yes , I can do that')
+            music_dir = "D:\Musics"
+            songs = os.listdir(music_dir)
             rd = random.choice(songs)
             for song in songs:
                 if song.endswith('.mp3'):
                     os.startfile(os.path.join(music_dir, song))
 
 
-        elif "ip address" in query: #Ip address of the particular network!
+
+        elif "ip address" in query:
             ip = get('https://api.ipify.org').text
-            speak(f'your ip address is {ip}')
+            speak(f"your IP address is {ip}")
 
+        elif "wikipedia" in query:
+            speak("searching wikipedia....")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("according to wikipedia")
+            speak(results)
+            # print(results)
 
-
-        elif "open Youtube" in query:
+        elif "open youtube" in query:
             webbrowser.open("www.youtube.com")
 
         elif "open facebook" in query:
             webbrowser.open("www.facebook.com")
 
-        elif "Search google" in query:
-            speak('What should i search in google')
-            cm = take_commnad().lower()
+        elif "open stackoverflow" in query:
+            webbrowser.open("www.stackoverflow.com")
+
+        elif "search google" in query:
+            speak("sir, what should i search on google")
+            cm = takecommand().lower()
             webbrowser.open(f"{cm}")
 
-        elif "Play song on youtube" in query:
-            pywhatkit.playonyt("little girl")
+        elif "close notepad" in query:
+            speak('closing notepad')
+            os.system("taskkill /f /im notepad.exe")
 
-        elif "send email" in query:
+        elif "close cmd" in query:
+            speak('closing cmd')
+            os.system("taskkill /f /im cmd.exe")
+
+        elif "tell me a joke" in query:
+            speak('you want the joke , in which language')
+            l = takecommand().lower()
+            if "english" in l:
+                joke = pyjokes.get_joke("en")
+                speak(joke)
+            elif "german" in l:
+                joke = pyjokes.get_joke("de")
+                speak(joke)
+
+            elif "spanish" in l:
+                joke = pyjokes.get_joke("es")
+                speak(joke)
+
+
+
+
+        elif "song on youtube" in query:
+            kit.playonyt("see you again")
+
+        elif 'timer' in query or 'stopwatch' in query:
+            speak("For how many minutes?")
+            timing = takecommand()
+            timing = timing.replace('minutes', '')
+            timing = timing.replace('minute', '')
+            timing = timing.replace('for', '')
+            timing = float(timing)
+            timing = timing * 60
+            speak(f'I will remind you in {timing} seconds')
+
+            time.sleep(timing)
+            speak('Your time has been finished sir')
+
+        elif "email to avinash" in query:
             try:
-                speak('what should i write in the email')
-                content = take_commnad().lower()
-                to = ""
-                SendEmail(to,content)
-                speak('Email has been send')
+                speak("what should i say?")
+                content = takecommand().lower()
+                to = "EMAIL OF THE OTHER PERSON"
+                sendEmail(to, content)
+                speak("Email has been sent to avinash")
 
             except Exception as e:
                 print(e)
-                speak('Sorry , I was not able to send the Email')
+                speak("sorry sir, i am not able to sent this mail to avi")
 
-        elif "close" in query:
-            speak('thanks for using me')
+        elif "Okay you can Quit" in query:
+            speak("thanks for using me sir, have a good day.")
             sys.exit()
 
-        speak('Can I help you ,  with Something else')
-
-
-
-
-
+        # speak("sir, do you have any other work")
