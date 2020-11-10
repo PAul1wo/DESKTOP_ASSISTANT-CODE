5b1f342e365565edf01601669c86ab80bc4412cb
PAul1wo <67853781+PAul1wo@users.noreply.github.com>
2020-11-10 19:50:14 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -1,24 +1,31 @@
 import pyttsx3
 import speech_recognition as sr
 import datetime
+import winshell
+import pytesseract
 import os
+import pyperclip
+from googletrans import Translator
 import cv2
 import random
+import instalooter
 from requests import  get
 import wikipedia
 import webbrowser
-
+import requests
 import smtplib
 import sys
 import time
 import pyjokes
-
-
 import pyautogui
+import json
+import geocoder
+import pyautogui
+import wolframalpha
+import PyPDF2
 
 
-
-
+g = geocoder.ip('me')
 
 engine = pyttsx3.init('sapi5')          #text to voice
 voices = engine.getProperty('voices')
@@ -52,17 +59,58 @@ def takecommand():
     return query
 
 
+def readfile():
+    t = open("a.txt","r")
+    r = t.read()
+    speak(r)
+    t.close()
+
+def pdf_reader():
+    book = open('Data Structures and Algorithms Made Easy_ Data Structures and Algorithmic Puzzles ( PDFDrive ).pdf','rb')
+    pdfReader = PyPDF2.PdfFileReader(book)
+    pages = pdfReader.numPages
+    speak(f'Total Number of Pages in this book is {pages}')
+    speak('enter the page number i have to read')
+    pg = int(input('Please Enter the page Number : '))
+    page = pdfReader.getPage(pg)
+    text = page.extractText()
+    speak(text)
+
+def locate():
+    place=query[1]
+    speak(f"according to my data base {place} lies here")
+    webbrowser.open_new_tab("https://www.google.com/maps/place/"+place)
 
 
+def weather(): #for Weather, Location , Lattitude, Longitude, Humidity, temperature
+    api_url = "https://fcc-weather-api.glitch.me/api/current?lat=" + \
+        str(g.latlng[0]) + "&lon=" + str(g.latlng[1])
 
+    data = requests.get(api_url)
+    data_json = data.json()
+    if data_json['cod'] == 200:
+        main = data_json['main']
+        wind = data_json['wind']
+        weather_desc = data_json['weather'][0]
+        speak(str(data_json['coord']['lat']) + 'latitude' + str(data_json['coord']['lon']) + 'longitude')
+        speak('Current location is ' + data_json['name'] + data_json['sys']['country'] + 'dia')
+        speak('weather type ' + weather_desc['main'])
+        speak('Wind speed is ' + str(wind['speed']) + ' metre per second')
+        speak('Temperature: ' + str(main['temp']) + 'degree celcius')
+        speak('Humidity is ' + str(main['humidity']))
 
+def news():
+    api_url1 = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apikey=f439165f85a3440c84b7f0d1552e2c32"
 
-
-def screenshot():
-    pyautogui.keyDown("windows")
-    pyautogui.press("Prtsc")
-    pyautogui.keyUp("windows")
-
+    page = requests.get(api_url1).json()  # we scrap the Data from the Url by json and put the data in the variable page
+    articles = page["articles"]  # Only taking the Articles from the page.
+    head = []
+    day = ["first","second","third","fourth","fifth"]
+    for ar in articles:
+        head.append(ar["title"]) #Fro Articles we take only the titles.
+    for i in range(len(day)):
+        speak(f"today's {day[i]} , your latest news are :{head[i]}")
+    speak("that were today's five latest news, hope they were informative")
 
 
 # to wish
@@ -76,7 +124,24 @@ def wish():
         speak(f"good afternoon, its {tt}")
     else:
         speak(f"good evening, its {tt}")
-    speak("i am jarvis sir. please tell me how may i help you")
+    speak("i am a Desktop Assistant. please tell me how may i help you")
+
+
+def langtranslator():
+    try:
+        trans = Translator()
+
+        speak("Say the language to translate in")
+        language = takecommand().replace(" ", "")
+
+        speak("what to translate")
+        content = takecommand()
+
+        t = trans.translate(text=content, dest=language)
+        print(f"{t.origin} in {t.dest} is , {t.text}")
+
+    except:
+        speak("error")
 
 
 # to send email
@@ -84,8 +149,11 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('YOUR EMAIL ADDRESS', 'YOUR PASSWORD')
-    server.sendmail('YOUR EMAIL ADDRESS', to, content)
+    email = "sasangkasarma7@gmail.com"
+    password = input()
+    server.login(email, password)
+    send_email = input()
+    server.sendmail(send_email, to, content)
     server.close()
 
 
@@ -102,17 +170,86 @@ if __name__ == "__main__":  # main program
             npath = "C:\\Windows\\system32\\notepad.exe"
             os.startfile(npath)
 
-        elif 'hi' in query or 'hello' in query:
-            speak('Hello sir, how may I help you?')
+        if "where is" in query:
+            query = query.split("where is")
+            locate()
 
-        elif 'take screenshot' in query:
-            screenshot()
-            speak('Screenshot saved in pictures folder')
+        elif 'can you remember this' in query:
+            speak("what should i remember sir")
+            rememberMessage = takecommand()
+            speak(f"you said me to remember {rememberMessage}")
+            remember = open('data.txt', 'w')
+            remember.write(rememberMessage)
+            remember.close()
 
-        elif "open command prompt" in query:
+        if "translate to" in query:
+            text = query.split("translate to")
+            dest = text[1]
+            langtranslator()
+
+
+        if "where am i" in query or "what is my location now" in query:
+            speak('please wait, i am checking')
+            try:
+                ipadd = requests.get('https://api.ipify.org').text
+                #print(ipadd)
+                url = 'https://get.geojs.io/v1/ip/geo/'+ipadd+'.json'
+                geo_requests = requests.get(url)
+                geo_data = geo_requests.json()
+                city = geo_data['city']
+                country = geo_data['country']
+                speak(f'i am not sure but i think we are in {city} in {country} country')
+
+            except Exception as e:
+                speak('Sorry')
+                pass
+
+        elif 'read book' in query:
+            pdf_reader()
+
+
+        elif "instagram profile" in query or "profile on instagram" in query:
+            speak('please, enter the instagram username correctly')
+            name = input('Enter the Username here : ')
+            webbrowser.open(f'www.instagram.com/{name}')
+            speak(f'here is the Profile with username {name}')
+            time.sleep(5)
+            speak('would you like to download profile picture of this account ')
+            condition = takecommand().lower()
+            if "yes" in condition:
+                mod = instalooter.Instaloader()   #Error here!
+                mod.download_profile(name,profile_pic_only=True)
+                speak('i am done')
+            else:
+                pass
+
+
+
+
+        elif 'do you remember anything' in query:
+            remember = open('data.txt', 'r')
+            t = remember.read()
+            speak(f"you said me to remember that {t}")
+            remember.close()
+
+        
+        elif "screenshot" in query or "take screenshot" in query:  #Working fine
+            speak('please , tell me the name you want for the screenshot file')
+            name = takecommand().lower()
+            speak('hold on , taking screenshot')
+            time.sleep(3)
+            img = pyautogui.screenshot()
+            img.save(f"{name}.png")
+            speak('screenshot is saved in our main folder , please check')
+
+
+        elif "bones" in query:
+            speak('206 , that was easy')
+
+        elif "open command prompt" in query or "open CMD" in query:
             os.system("start cmd")
 
-        elif "open camera" in query:
+        elif "open camera" in query or "open webcam" in query:
             cap = cv2.VideoCapture(0)  # 0 for internal Camera
             while True:
                 ret, img = cap.read()
@@ -124,17 +261,56 @@ if __name__ == "__main__":  # main program
             cv2.destroyAllWindows()
 
 
+
+
+        elif "weather" in query:
+            weather()
+
+        elif "tell me latest news" in query:
+            news()
+
+
+
+
+        elif "switch window please" in query:
+            speak('Switching to Previous Window')
+            pyautogui.keyDown("alt")
+            pyautogui.press("tab")
+            time.sleep(1)
+            pyautogui.keyUp("alt")
+
+
         elif "play music" in query:
             speak('Yes , I can do that')
-            music_dir = "D:\Musics"
+            music_dir = "D:\\Musics"
             songs = os.listdir(music_dir)
             rd = random.choice(songs)
             for song in songs:
                 if song.endswith('.mp3'):
                     os.startfile(os.path.join(music_dir, song))
 
-        elif 'take a screenshot' in query:
-            screenshot()
+        elif 'read a file' in query:
+            readfile()
+
+        elif 'how are you' in query or 'how are you doing' in query:
+            speak("am fine , what about you?")
+            query = takecommand()
+            if 'am also good' in query or 'am also fine' in query or 'healthy' in query or 'fine' in query:
+                speak("wow")
+            if 'not well' in query or 'not good' in query or 'felling low' in query or 'not in mood' in query:
+                speak("sad to hear that sir, how may I change your mood, May i play music for You?")
+                query = takecommand()
+                if 'ok' in query or 'sure' in query or 'hmm' in query or 'alright' in query or 'yeah' in query or 'play music' in query:
+                    speak('ok , playing music for you')
+                    music_dir = 'D:\\Musics'
+                    songs = os.listdir(music_dir)   # Error Here
+                    rd = random.choice(songs)
+                    print(songs)
+                    for songs in songs:
+                        if songs.endswith('.mp3'):
+                            os.startfile(os.path.join(music_dir, songs))
+                elif "no" in query or "it's ok" in query or "don't play" in query or 'nope' in query:
+                    speak("Ok , as You like!")
 
 
         elif "ip address" in query:
@@ -144,10 +320,15 @@ if __name__ == "__main__":  # main program
         elif "wikipedia" in query:
             speak("searching wikipedia....")
             query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2,chars=10, auto_suggest=True, redirect=True)
+            results = wikipedia.summary(query, sentences=1,chars=10, auto_suggest=True, redirect=True)
             speak("according to wikipedia")
             speak(results)
             # print(results)
+
+        elif 'what is the time' in query:
+            hour = int(datetime.datetime.now().hour)  # Current time
+            tt = time.strftime("%I:%M %p")
+            speak(f'its {tt}')
 
         elif "open youtube" in query:
             webbrowser.open("www.youtube.com")
@@ -167,22 +348,9 @@ if __name__ == "__main__":  # main program
             speak('Closing Facebook Now')
             os.system("taskkill /f /im chrome.exe")
 
-        elif "calculator" or "calulate this for me" in query:
-            speak('what operation you want to do')
-            n = takecommand()
-            if 'addition' or 'add' in n:
-                speak('please , give me the two numbers ')
-                n1 = takecommand()
-                n2 = takecommand()
-                add1 = int(n1) + int(n2)
-                speak(f'your answer is {add1}')
 
-            elif 'substraction' or 'substract' in n:
-                speak('please , give me the two numbers ')
-                n1 = takecommand()
-                n2 = takecommand()
-                sub1 = int(n1) - int(n2)
-                speak(f'the answer is {sub1}')
+
+
 
 
         elif "close youtube" in query:
@@ -202,7 +370,7 @@ if __name__ == "__main__":  # main program
             speak('closing cmd')
             os.system("taskkill /f /im cmd.exe")
 
-        elif "tell me a joke" or "can you tell me a joke" or "i want a joke" in query:
+        elif  "can you tell me a joke"  in query or "tell me a joke" in query or "i am sad tell a joke" in query:
             speak('you want the joke , in which language')
             l = takecommand().lower()
             if "english" in l:
@@ -215,6 +383,7 @@ if __name__ == "__main__":  # main program
             elif "spanish" in l:
                 joke = pyjokes.get_joke("es")
                 speak(joke)
+
 
 
      #   elif "song on youtube" in query:
@@ -234,20 +403,27 @@ if __name__ == "__main__":  # main program
             speak(f'Your {timing} has been finished sir')
 
 
-        elif "email to avinash" in query:
+        elif "email" in query:
             try:
                 speak("what should i say?")
                 content = takecommand().lower()
                 to = "EMAIL OF THE OTHER PERSON"
                 sendEmail(to, content)
-                speak("Email has been sent to avinash")
+                speak("Email has been sent")
 
             except Exception as e:
                 print(e)
-                speak("sorry sir, i am not able to sent this mail to avi")
+                speak("sorry , i am not able to sent this mail ")
 
-        elif "you can quit now" in query:
-            speak("thanks for using me , have a good day.")
+        elif "you can quit now" in query or "quit" in query or "goodbye" in query:
+            speak("thanks for using me ")
+            hour = int(datetime.datetime.now().hour)
+            if hour >= 0 and hour < 18:
+                speak("Have a Nice day !")
+                exit()
+            elif hour >= 18 and hour < 24:
+                speak("good Night ")
+
             sys.exit()
 
         #speak("Can I help with , with something else")
